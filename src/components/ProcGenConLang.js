aaf15a18a7ee6348d9fd0ab4005a9130e81887a2
feelic <feelics@gmail.com>
2021-01-17 18:16:13 +0100

    show phrasebook in pgcl page

--- src/components/ProcGenConLang.js
+++ src/components/ProcGenConLang.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { generateLanguage } from "../gobbledygook/generate-language/index";
-import { transliterate } from "../gobbledygook/use-language";
+import { transliterate, makeSentence } from "../gobbledygook/use-language";
+import sentences from "../gobbledygook/sample-sentences/index";
 import AudioButton from "./AudioButton";
 import { VOICES, DEFAULT_VOICE } from "../constants/voices";
 
@@ -32,16 +33,21 @@ export default function ProcGenConLang() {
         }}
       />
     <select onChange={(e) => {
-          console.log(e.target.value)
           setVoice(e.target.value)
         }} value={voice}>
         {Object.keys(VOICES).map((v) => (
           <option value={v} key={v}>{VOICES[v]}</option>
         ))}
       </select>
+      <h3>{transliterate(lang, lang.name)} phrase book</h3>
+      {sentences.map((sentence) => {
+        return <Sentence key={sentence.transcript} lang={lang} sentence={sentence} voice={voice} />
+      })}
+      <h3>{transliterate(lang, lang.name)} dictionary</h3>
       <ul>
         {Object.entries(lang.morphemeDictionary).map(([meaning, word]) => {
           const translit = transliterate(lang, word);
+
           return (
             <li key={meaning}>
               {meaning}: {translit} ( /{word}/ ) <AudioButton sentence={word} voice={voice} />
@@ -49,6 +55,28 @@ export default function ProcGenConLang() {
           );
         })}
       </ul>
+      {sentences.map((sentence) => {
+        return <Sentence key={sentence.transcript} lang={lang} sentence={sentence} voice={voice} />
+      })}
     </section>
   );
+}
+
+function Sentence ({lang, sentence, voice}) {
+  try {
+    const formedSentence = makeSentence(lang, sentence);
+
+    return (
+      <div className="sentenceBlock">
+        <p>
+          {transliterate(lang, formedSentence)}{" "}
+          <AudioButton sentence={formedSentence} voice={voice} />
+        </p>
+      </div>
+    );
+  } catch {
+    return <p>Couldn't say <span className="originalSentence">"{sentence.transcript}"</span> in {transliterate(lang, lang.name)}</p>
+  }
+
+
 }
